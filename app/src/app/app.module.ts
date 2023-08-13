c7bd785abd757bdef29b331e498a9b6fbf9498ea
Zheng Wang <cerebronzw@gmail.com>
2023-08-12 22:39:22 -0400

    setup template

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { FeatureComponent } from './feature/feature.component';
+import { CtaComponent } from './cta/cta.component';
+import { ServiceComponent } from './service/service.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    FeatureComponent,
+    CtaComponent,
+    ServiceComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
